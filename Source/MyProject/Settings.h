a4aec78d230c1c6950b1b05cd100d4904421f9a1
Kai Hirota <kaiyhirota@gmail.com>
2022-03-25 22:34:14 +1100

    added streams ARN

--- Source/MyProject/Settings.h
+++ Source/MyProject/Settings.h
@@ -1,10 +1,10 @@
 #pragma once
 
 #include "CoreMinimal.h"
-// #include <aws/core/Region.h>
 
 // const char ACCT_ID[] = "AKIA6BPFQACFZTW2QD4K";
 // // const char POOL_ID[] = "us-east-1:xx0000x0-0x00-0000-0000-0x0000000xxx";
 // const char* REGION = Aws::Region::AP_SOUTHEAST_2;
 
 const FString MarkerAPIEndpoint = "https://0flacbdme3.execute-api.ap-southeast-2.amazonaws.com/dev/";
+const FString DynamoDBStreamsARN = "arn:aws:dynamodb:ap-southeast-2:965238325387:table/mojexa-markers/stream/2022-03-21T07:34:06.673";
