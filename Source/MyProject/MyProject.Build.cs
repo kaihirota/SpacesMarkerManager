e6b938bad14739a413731d9ea784123464b4cf63
Kai Hirota <kaiyhirota@gmail.com>
2022-03-31 19:20:28 +1100

    add dependency

--- Source/MyProject/MyProject.Build.cs
+++ Source/MyProject/MyProject.Build.cs
@@ -6,9 +6,8 @@ public class MyProject : ModuleRules
 {
 	public MyProject(ReadOnlyTargetRules Target) : base(Target)
 	{
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "HTTP", "Json", "JsonUtilities", "AWSCoreLibrary", "DynamoDBClientLibrary", "DynamoDBStreamsClientLibrary"});
 		PrivateDependencyModuleNames.AddRange(new string[] { "JsonUtilities" });
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "HTTP", "Json", "JsonUtilities"});
 	}
 }
