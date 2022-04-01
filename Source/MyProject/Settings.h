9b28a02118911a8cf83f09056f7f4bc2e349f83c
Kai Hirota <kaiyhirota@gmail.com>
2022-04-01 20:29:58 +1100

    update

--- Source/MyProject/Settings.h
+++ Source/MyProject/Settings.h
@@ -10,3 +10,4 @@ const FString MarkerAPIEndpoint = "https://0flacbdme3.execute-api.ap-southeast-2
 const Aws::String DynamoDBStreamsARN = "arn:aws:dynamodb:ap-southeast-2:965238325387:table/mojexa-markers/stream/2022-03-21T07:34:06.673";
 const Aws::String AWSAccessKeyId = "AKIA6BPFQACFZTW2QD4K";
 const Aws::String AWSSecretKey = "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";
+const Aws::String DynamoDBTableName = "mojexa-markers";
