c9e3ba3f29cbd67bcf559cca30aad54d086cf9a1
Kai Hirota <kaiyhirota@gmail.com>
2022-04-05 08:22:39 +1000

    add gameinstance class

---
+++ Source/MyProject/MojexaSpacesMarkerManager.cpp
@@ -0,0 +1,316 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MojexaSpacesMarkerManager.h"
+
+#include "DynamicMarker.h"
+#include "HttpModule.h"
+#include "Settings.h"
+#include "TemporaryMarker.h"
+#include "aws/core/Aws.h"
+#include "aws/core/auth/AWSCredentials.h"
+#include "aws/core/client/ClientConfiguration.h"
+#include "aws/dynamodb/DynamoDBClient.h"
+#include "aws/dynamodb/model/QueryRequest.h"
+#include "Interfaces/IHttpRequest.h"
+#include "Interfaces/IHttpResponse.h"
+#include "Misc/DefaultValueHelper.h"
+
+UMojexaSpacesMarkerManager::UMojexaSpacesMarkerManager(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	
+}
+
+void UMojexaSpacesMarkerManager::Init()
+{
+	Super::Init();
+	UE_LOG(LogTemp, Warning, TEXT("Game instance initializing"));
+	Aws::InitAPI(Aws::SDKOptions());
+	const Aws::Auth::AWSCredentials Credentials = Aws::Auth::AWSCredentials(AWSAccessKeyId, AWSSecretKey);
+	Aws::Client::ClientConfiguration Config = Aws::Client::ClientConfiguration();
+	Config.region = REGION;
+	ClientRef = new Aws::DynamoDB::DynamoDBClient(Credentials, Config);
+	UE_LOG(LogTemp, Warning, TEXT("Game instance initialized"));
+}
+
+void UMojexaSpacesMarkerManager::Shutdown()
+{
+	Super::Shutdown();
+	Aws::ShutdownAPI(Aws::SDKOptions());
+}
+
+
+FVector UMojexaSpacesMarkerManager::GetLatestRecord(FString TargetDeviceID, FDateTime LastKnownTimestamp)
+{
+	Aws::DynamoDB::Model::QueryRequest Request;
+	Request.SetTableName(DynamoDBTableName);
+	Request.SetKeyConditionExpression("device_id = :valueToMatch");
+
+	// Set Expression AttributeValues
+	Aws::Map<Aws::String, Aws::DynamoDB::Model::AttributeValue> AttributeValues;
+	Aws::String PartitionKeyVal = Aws::String(TCHAR_TO_UTF8(*TargetDeviceID));
+	AttributeValues.emplace(":valueToMatch", PartitionKeyVal);
+	Request.SetExpressionAttributeValues(AttributeValues);
+	Request.SetScanIndexForward(false);
+	Request.SetLimit(1);
+
+	// Perform Query operation
+	const Aws::DynamoDB::Model::QueryOutcome& result = ClientRef->Query(Request);
+	if (result.IsSuccess())
+	{
+		// Reference the retrieved items
+		for(const auto &Item: result.GetResult().GetItems())
+		{
+			// get timestamp data
+			Aws::DynamoDB::Model::AttributeValue TimestampValue = Item.at("created_timestamp");
+			FDateTime Timestamp = FDateTime::FromUnixTimestamp(std::atoi(TimestampValue.GetS().c_str()));
+			UE_LOG(LogTemp, Warning, TEXT("Timestamp: %d"), Timestamp.ToUnixTimestamp());
+
+			if (Timestamp > LastKnownTimestamp)
+			{
+				// get location data
+				float lon, lat, elev;
+				FDefaultValueHelper::ParseFloat(FString(Item.at("longitude").GetS().c_str()), lon);
+				FDefaultValueHelper::ParseFloat(FString(Item.at("latitude").GetS().c_str()), lat);
+				FDefaultValueHelper::ParseFloat(FString(Item.at("elevation").GetS().c_str()), elev);
+				return FVector(lon, lat, elev);
+			}
+		}
+	}
+	UE_LOG(LogTemp, Warning, TEXT("Failed to query items: %s"), *FString(result.GetError().GetMessage().c_str()));
+	return FVector::ZeroVector;
+}
+
+ALocationMarker* UMojexaSpacesMarkerManager::CreateMarkerFromJsonObject(const FJsonObject* JsonObject)
+{
+	const FDateTime Timestamp = FDateTime::FromUnixTimestamp(FCString::Atoi(*JsonObject->GetStringField("created_timestamp")));
+	FString MarkerTypeStr;
+	ELocationMarkerType MarkerType = ELocationMarkerType::Static;
+	
+	if (JsonObject->TryGetStringField("marker_type", MarkerTypeStr))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Marker type Field: %s"), *MarkerTypeStr);
+		if (MarkerTypeStr == FString("dynamic"))
+		{
+			MarkerType = ELocationMarkerType::Dynamic;
+		}
+	}
+	return CreateMarker(FVector(
+		JsonObject->GetNumberField("longitude"),
+		JsonObject->GetNumberField("latitude"),
+		JsonObject->GetNumberField("elevation")),
+		Timestamp,
+		JsonObject->GetStringField("device_id"),
+		MarkerType);
+}
+
+ALocationMarker* UMojexaSpacesMarkerManager::CreateMarker(const FVector SpawnLocation, const ELocationMarkerType MarkerType)
+{
+	return CreateMarker(SpawnLocation, FDateTime::Now(), FString("UE"), MarkerType);
+}
+
+ALocationMarker* UMojexaSpacesMarkerManager::CreateMarker(const FVector SpawnLocation, const FDateTime Timestamp, const FString DeviceID, const ELocationMarkerType MarkerType)
+{
+	ALocationMarker* Marker = nullptr;
+	if (!AllMarkers.Contains(GetTypeHash(SpawnLocation, Timestamp, DeviceID)))
+	{
+		AActor* MarkerActor;
+
+		switch (MarkerType)
+		{
+			case ELocationMarkerType::Static:
+				MarkerActor = GetWorld()->SpawnActor<ALocationMarker>(SpawnLocation, FRotator::ZeroRotator);
+				break;
+			case ELocationMarkerType::Temporary:
+				MarkerActor = GetWorld()->SpawnActor<ATemporaryMarker>(SpawnLocation, FRotator::ZeroRotator);
+				break;
+			case ELocationMarkerType::Dynamic:
+				MarkerActor = GetWorld()->SpawnActor<ADynamicMarker>(SpawnLocation, FRotator::ZeroRotator);
+				ADynamicMarker* DynamicMarker = Cast<ADynamicMarker>(MarkerActor);
+				DynamicMarker->ManagerDelegate.BindUFunction(this, FName("GetLatestRecord"));
+				break;
+		}
+		
+		Marker = Cast<ALocationMarker>(MarkerActor);
+		if (Marker != nullptr)
+		{
+			// TODO convert UE coord to WGS84
+			Marker->Coordinate = SpawnLocation;
+			Marker->Timestamp = Timestamp;
+			Marker->DeviceID = DeviceID;
+
+			UE_LOG(LogTemp, Warning, TEXT("Created %s - %s"), *Marker->GetName() , *Marker->ToString());
+			UE_LOG(LogTemp, Warning, TEXT("%s"), *Marker->ToJsonString());
+		}
+		
+		AllMarkers.Add(GetTypeHash(Marker), Marker);
+		UE_LOG(LogTemp, Warning, TEXT("%d data points stored"), AllMarkers.Num());
+	}
+	return Marker;
+}
+
+bool UMojexaSpacesMarkerManager::CreateMarkerInDB(ALocationMarker* Marker)
+{
+	FHttpModule* Http = &FHttpModule::Get();
+	if (Http != nullptr)
+	{
+		const TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request = Http->CreateRequest();
+		// Request->OnProcessRequestComplete().BindUObject(this, &ThisClass::OnGetMarkersResponse);
+		Request->SetURL(MarkerAPIEndpoint);
+		Request->SetVerb("POST");
+		Request->SetHeader("User-Agent", "X-UnrealEngine-Agent");
+		Request->SetHeader("Content-Type", TEXT("application/json"));
+		Request->SetHeader(TEXT("Accepts"), TEXT("application/json"));
+		Request->SetContentAsString(Marker->ToJsonString());
+		Request->ProcessRequest();
+		UE_LOG(LogTemp, Warning, TEXT("Created POST Markers request successfully."));
+		return true;
+	}
+	UE_LOG(LogTemp, Warning, TEXT("Failed to create POST request"));
+	return false;
+}
+
+void UMojexaSpacesMarkerManager::GetMarkersFromDB()
+{
+	FHttpModule* Http = &FHttpModule::Get();
+	if (Http != nullptr)
+	{
+		const TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request = Http->CreateRequest();
+		Request->OnProcessRequestComplete().BindUObject(this, &ThisClass::OnGetMarkersResponse);
+		Request->SetURL(MarkerAPIEndpoint);
+		Request->SetVerb("GET");
+		Request->SetHeader("User-Agent", "X-UnrealEngine-Agent");
+		Request->SetHeader("Content-Type", "application/json");
+		Request->ProcessRequest();
+		UE_LOG(LogTemp, Log, TEXT("Created GET Markers request successfully."));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Failed to create GET request"));
+	}
+}
+
+void UMojexaSpacesMarkerManager::OnGetMarkersResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
+{
+	TSharedPtr<FJsonObject> JsonObjArr;
+	if(Response->GetResponseCode() == 200 && bWasSuccessful)
+	{
+		const FString ResponseBody = Response->GetContentAsString();
+		TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(ResponseBody);
+		if(FJsonSerializer::Deserialize(Reader, JsonObjArr))
+		{
+			TArray<TSharedPtr<FJsonValue>> MarkerArray = JsonObjArr->GetArrayField("data");
+			UE_LOG(LogTemp, Warning, TEXT("Fetch Success: %d items"), MarkerArray.Num());
+			// for dynamic markers, spawn the last location
+			TMap<FString, FJsonObject*> DynamicMarkers;
+			
+			for(const TSharedPtr<FJsonValue>& MarkerValue : MarkerArray)
+			{
+				// how to determine if new marker should be spawned, or not?
+				// no marker with same device id, time, and coordinates can co exist
+				FJsonObject* JsonObject = MarkerValue->AsObject().Get();
+				if(JsonObject->HasField("marker_type") && JsonObject->HasField("device_id") && JsonObject->HasField("created_timestamp"))
+				{
+					FString MarkerType = JsonObject->GetStringField("marker_type");// == FString("dynamic")
+					FString DeviceID = JsonObject->GetStringField("device_id");
+					
+					FString TimestampStr = JsonObject->GetStringField("created_timestamp");
+					FDateTime Timestamp = FDateTime::FromUnixTimestamp(FCString::Atoi(*TimestampStr));
+					UE_LOG(LogTemp, Log, TEXT("Timestamp: %s"), *Timestamp.ToString());
+					DynamicMarkers.Add(DeviceID, JsonObject);
+				} else
+				{
+					const ALocationMarker* Marker = CreateMarkerFromJsonObject(JsonObject);
+					if (Marker != nullptr) UE_LOG(LogTemp, Log, TEXT("Created Marker: %s %s"), *Marker->GetName(), *Marker->ToString());
+				}
+			}
+
+			for (const auto DMarker : DynamicMarkers)
+			{
+				const ALocationMarker* Marker = CreateMarkerFromJsonObject(DMarker.Value);
+				if (Marker != nullptr) UE_LOG(LogTemp, Log, TEXT("Created Dynamic Marker: %s %s"), *Marker->GetName(), *Marker->ToString());
+			} 
+			
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Error %d: %s"), Response->GetResponseCode(), *Response->GetContentAsString());
+	}
+}
+
+void UMojexaSpacesMarkerManager::DeleteMarker(ALocationMarker* Marker, bool DeleteFromDB)
+{
+	if (Marker != nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Destroying: %s - %s"), *Marker->GetName(), *Marker->ToString());
+		Marker->Destroy();
+
+		if (DeleteFromDB)
+		{
+			DeleteMarkerFromDB(Marker);
+		} 
+		AllMarkers.FindAndRemoveChecked(GetTypeHash(Marker));
+	}
+}
+
+void UMojexaSpacesMarkerManager::DeleteMarkers(const bool DeleteFromDB)
+{
+	for(const auto &Pair: AllMarkers)
+	{
+		ALocationMarker* Marker = Pair.Value;
+		if (Marker != nullptr)
+		{
+			DeleteMarker(Marker, DeleteFromDB);
+		}
+	}
+}
+
+void UMojexaSpacesMarkerManager::DeleteSelectedMarkers(bool DeleteFromDB)
+{
+	for (auto It = AllMarkers.CreateIterator(); It; ++It)
+	{
+		ALocationMarker* Marker = It.Value();
+		if (Marker != nullptr)
+		{
+			if (Marker->Selected)
+			{
+				UE_LOG(LogTemp, Warning, TEXT("Destroying: %s - %s"), *Marker->GetName(), *Marker->ToString());
+				DeleteMarkerFromDB(Marker);
+				if (DeleteFromDB) Marker->Destroy();
+				It.RemoveCurrent();
+			} 
+		}
+	}
+}
+
+void UMojexaSpacesMarkerManager::DeleteMarkerFromDB(ALocationMarker* Marker)
+{
+	FHttpModule* Http = &FHttpModule::Get();
+	if (Http != nullptr)
+	{
+		const TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request = Http->CreateRequest();
+		Request->OnProcessRequestComplete().BindUObject(this, &ThisClass::OnDeleteMarkersResponse);
+		Request->SetURL(MarkerAPIEndpoint);
+		Request->SetVerb("DELETE");
+		Request->SetHeader("User-Agent", "X-UnrealEngine-Agent");
+		Request->SetHeader("Content-Type", "application/json");
+		Request->SetContentAsString(Marker->ToJsonString());
+		Request->ProcessRequest();
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Failed to delete marker: %s"), *Marker->ToJsonString());
+	}
+}
+
+void UMojexaSpacesMarkerManager::OnDeleteMarkersResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
+{
+	if(Response->GetResponseCode() != 200 || !bWasSuccessful)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Could not delete marker - HTTP %d: %s"), Response->GetResponseCode(), *Response->GetContentAsString());
+	} else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("SUCCESS: Delete marker - HTTP %d: %s"), Response->GetResponseCode(), *Response->GetContentAsString());
+	}
+}
---
+++ Source/MyProject/MojexaSpacesMarkerManager.h
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "aws/dynamodb/DynamoDBClient.h"
+#include "Interfaces/IHttpRequest.h"
+#include "MojexaSpacesMarkerManager.generated.h"
+
+UENUM()
+enum class ELocationMarkerType : uint8
+{
+	Static,
+	Temporary,
+	Dynamic
+};
+
+class ALocationMarker;
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API UMojexaSpacesMarkerManager : public UGameInstance
+{
+public:
+	// UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TMap<uint32, ALocationMarker*> AllMarkers;
+
+	Aws::DynamoDB::DynamoDBClient* ClientRef;
+private:
+	GENERATED_BODY()
+
+protected:
+	void OnGetMarkersResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+	void OnDeleteMarkersResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+public:
+	UMojexaSpacesMarkerManager(const FObjectInitializer& ObjectInitializer);
+	virtual void Init() override;
+	virtual void Shutdown() override;
+	
+	UFUNCTION(BlueprintCallable)
+	ALocationMarker* CreateMarker(const FVector SpawnLocation, const ELocationMarkerType MarkerType);
+	ALocationMarker* CreateMarker(const FVector SpawnLocation, const FDateTime Timestamp, const FString DeviceID);
+	ALocationMarker* CreateMarker(const FVector SpawnLocation, const FDateTime Timestamp, const FString DeviceID, const ELocationMarkerType MarkerType);
+	ALocationMarker* CreateMarkerFromJsonObject(const FJsonObject* JsonObject);
+
+	UFUNCTION(BlueprintCallable)
+	bool CreateMarkerInDB(ALocationMarker* Marker);
+	
+	UFUNCTION(BlueprintCallable)
+	void GetMarkersFromDB();
+
+	UFUNCTION(BlueprintCallable)
+	FVector GetLatestRecord(FString TargetDeviceID, FDateTime LastKnownTimestamp);
+
+	UFUNCTION(BlueprintCallable)
+	void DeleteMarker(ALocationMarker* Marker, const bool DeleteFromDB = true);
+
+	UFUNCTION(BlueprintCallable)
+	void DeleteMarkers(const bool DeleteFromDB = true);
+	
+	UFUNCTION(BlueprintCallable)
+	void DeleteSelectedMarkers(bool DeleteFromDB = true);
+	
+	UFUNCTION(BlueprintCallable)
+	void DeleteMarkerFromDB(ALocationMarker* Marker);
+};
+
